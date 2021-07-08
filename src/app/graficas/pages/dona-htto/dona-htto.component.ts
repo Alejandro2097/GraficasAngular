ae93f9c3c22afbc89f01fe3507cf4861481925a7
Alejandro2097 <alejo12381@hotmail.es>
2021-07-08 10:29:32 -0500

    final

--- src/app/graficas/pages/dona-htto/dona-htto.component.ts
+++ src/app/graficas/pages/dona-htto/dona-htto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
+import { GraficasService } from '../../services/graficas.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dona-htto',
@@ -8,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonaHttoComponent implements OnInit {
 
-  constructor() { }
+  public doughnutChartLabels: Label[] = [
+    //'Download Sales', 'In-Store Sales', 'Mail-Order Sales'
+  ];
+  public doughnutChartData: MultiDataSet = [
+    //[350, 450, 100]
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+    {
+      backgroundColor:[
+        '#6C12FC',
+        '#9A10E0',
+        '#E91EF7',
+        '#E01092',
+        '#FC122E'
+      ]
+    }
+  ]
+  constructor(private graficaService: GraficasService) { }
 
   ngOnInit(): void {
+    //this.graficaService.getUsuariosRedesSociales()
+    //  .subscribe(data => {
+    //    console.log(data);
+    //    const labels = Object.keys(data);
+    //    const value = Object.values(data);
+    //    this.doughnutChartLabels = labels;
+    //    this.doughnutChartData.push(value);
+    //    console.log(value);
+    //    console.log(labels);
+    //  });
+    this.graficaService.getUsuariosRedesDonaData()
+      .subscribe(({labels, value}) => {
+        this.doughnutChartLabels = labels;
+        this.doughnutChartData.push(value);
+      })
   }
 
 }
--- src/app/graficas/services/graficas.service.ts
+++ src/app/graficas/services/graficas.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { map, delay } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class GraficasService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsuariosRedesSociales(){
+    return this.http.get('http://localhost:3000/grafica');
+  }
+  getUsuariosRedesDonaData(){
+    return this.getUsuariosRedesSociales()
+    .pipe(
+      delay(1500),
+      map(data => {
+        const labels = Object.keys(data);
+        const value = Object.values(data);
+        return { labels, value};
+      })
+    );
+  }
 }
